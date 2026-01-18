f9cdc1a8ce62722d9fbae455ee48c87df88c6990
Sam <sukhrobbekrustamov26@gmail.com>
2026-01-18 10:25:12 -0800

    html

--- todoApp/urls.py
+++ todoApp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import todo_list
+from .views import todo_list, completed_todos, pending_todos
 
 urlpatterns = [
-    path('', todo_list),
+    path('todos/', todo_list),
+    path('todos/completed', completed_todos, name="completed todos"),
+    path('todos/pending', pending_todos, name="pending todos")
 ]
--- todoApp/views.py
+++ todoApp/views.py
@@ -1,4 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Todo
 
 def todo_list(request):
-    pass
+    todos = Todo.objects.all()
+    
+    return render(request, 'todoApp/todo_list.html' , {'todos': todos})
+
+def pending_todos(request):
+    todos = todo.objects.filter(is_completed=False)
+    response = ""
+
+    for todo in todos:
+        response += f"{todos.title}\n{todo.priority}\n{todo.due_date}"
+    
+    return HttpResponse(response)
+
+def completed_todos(request):
+    todos = Todo.objects.filter(is_completed = True)
+    response = ""
+
+    for todo in todos:
+        response += f"{todo.title}\n"
+
+    return HttpResponse(response)
+    
