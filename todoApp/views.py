707cd918db2c034ebd7ae07c1d00a42d7f255b91
Sam <sukhrobbekrustamov26@gmail.com>
2026-02-07 10:04:41 -0800

    add accounts

--- todoApp/views.py
+++ todoApp/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from .models import Todo
 from .forms import TodoForm
 
+@login_required
 def todo_list(request):
     todos = Todo.objects.all()
     
     return render(request, 'todoApp/todo_list.html' , {'todos': todos})
 
+@login_required
 def pending_todos(request):
     todos = Todo.objects.filter(is_completed=False)
     return render(request, 'todoApp/pending_todos.html' , {'todos': todos})
 
+@login_required
 def completed_todos(request):
     todos = Todo.objects.filter(is_completed = True)
     return render(request, 'todoApp/completed_todos.html' , {'todos': todos})
